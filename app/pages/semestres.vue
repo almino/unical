dd198a8e8283d330a9e22b71f3662dd63fbf5460
Almino Melo <almino.melo@gmail.com>
2026-01-09 13:35:05 -0300

    Consigo gerar a timeline bonitinha

--- app/pages/semestres.vue
+++ app/pages/semestres.vue
@@ -1,12 +1,38 @@
 <script lang="ts" setup>
-const { data: semestres } = await useAsyncData('semestres', () => {
-  return queryCollection('semestres')
-    .order('semestre', 'DESC')
-    .all()
-})
+const { data: semestres } = await useAsyncData(
+  "semestres",
+  () => {
+    return queryCollection("semestres")
+      .order("semestre", "DESC")
+      .all();
+  }
+);
+
+const eventos = computed(() => {
+  return semestres.value.flatMap(
+    (semestre: any) => {
+      return semestre.eventos.map(
+        (evento: any) => ({
+          ...evento,
+          semestre: {
+            ...semestre,
+            meses_letivos: undefined,
+            eventos: undefined,
+          },
+        })
+      );
+    }
+  );
+});
+
+const items = computed(() => {
+  return eventos.value.map((evento: any) => ({
+    date: evento.data,
+    title: evento.texto,
+  }));
+});
 </script>
 
 <template>
-  {{ semestres }}
-  <p>teste</p>
+  <UTimeline :items="items" />
 </template>
