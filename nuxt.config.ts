9d2a7a5f8b008d0565491d2d9fb5d547be049338
Almino Melo <almino.melo@gmail.com>
2026-01-09 11:34:11 -0300

    `bun create nuxt@latest unical`

---
+++ nuxt.config.ts
@@ -0,0 +1,12 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  modules: [
+    '@nuxt/content',
+    '@nuxt/eslint',
+    '@nuxt/image',
+    '@nuxt/ui',
+    '@nuxt/a11y',
+  ],
+  devtools: { enabled: true },
+  compatibilityDate: '2024-04-03',
+})
