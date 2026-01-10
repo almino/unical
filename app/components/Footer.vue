a4cfe467eedf82763a19d5f2091f00ea3e7f89e8
Almino Melo <almino.melo@gmail.com>
2026-01-10 14:48:42 -0300

    Schema.org do projeto

--- app/components/Footer.vue
+++ app/components/Footer.vue
@@ -5,19 +5,37 @@ const appConfig = useAppConfig();
 <template>
   <UFooter>
     <template #default>
-      Feito com <a href="http://nuxt.com">
-        <UIcon name="logos:nuxt" class="inline-block mx-2" />
-      </a>
-      por
-      <UButton
-        icon="i-simple-icons-github"
-        color="neutral"
-        variant="ghost"
-        :to="appConfig.git"
-        target="_blank"
-        aria-label="GitHub"
-      />
-      Almino Melo
+      <div vocab="https://schema.org/" typeof="WPFooter">
+        <span property="text">
+          Feito com
+          <a
+            href="https://nuxt.com"
+            property="url"
+            typeof="SoftwareApplication"
+          >
+            <UIcon name="logos:nuxt" class="inline-block mx-2" />
+            <span class="hidden" property="name">Nuxt</span>
+          </a>
+          por
+          <span typeof="SoftwareSourceCode">
+            <span class="hidden" property="name">UniCal</span>
+            <UButton
+              icon="i-simple-icons-github"
+              color="neutral"
+              variant="ghost"
+              :to="appConfig.git"
+              target="_blank"
+              aria-label="GitHub"
+              property="codeRepository"
+            />
+          </span>
+          <span typeof="Person" property="author">
+            <ULink to="https://almino.dev.br" property="url">
+              <span property="name">Almino Melo</span>
+            </ULink>
+          </span>
+        </span>
+      </div>
     </template>
   </UFooter>
 </template>
