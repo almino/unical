5620d22b00de8201f3c0081f06ac8510cfc942d7
Almino Melo <almino.melo@gmail.com>
2026-01-10 09:57:32 -0300

    Organização da configuração

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,13 +1,13 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
+/* https://nuxt.com/docs/4.x/api/nuxt-config */
 export default defineNuxtConfig({
-  modules: [
-    '@nuxt/content',
-    '@nuxt/eslint',
-    '@nuxt/image',
-    '@nuxt/ui',
-    '@nuxt/a11y',
-  ],
-  css: ['~/assets/css/main.css'],
+  compatibilityDate: "2024-04-03",
+  css: ["~/assets/css/main.css"],
   devtools: { enabled: true },
-  compatibilityDate: '2024-04-03',
-})
+  modules: [
+    "@nuxt/content",
+    "@nuxt/eslint",
+    "@nuxt/image",
+    "@nuxt/ui",
+    "@nuxt/a11y",
+  ],
+});
