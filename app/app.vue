e117989eadf59cd7f75350c473d88df7b90fcbc7
Almino Melo <almino.melo@gmail.com>
2026-01-10 09:51:23 -0300

    Mais parecido com um PWA

--- app/app.vue
+++ app/app.vue
@@ -1,6 +1,25 @@
 <template>
   <NuxtRouteAnnouncer />
   <UApp>
-    <NuxtPage />
+    <UHeader />
+    <UMain>
+      <NuxtLayout>
+        <UContainer class="py-2 sm:py-4 lg:py-6">
+          <NuxtPage />
+        </UContainer>
+      </NuxtLayout>
+    </UMain>
+    <UFooter>
+      <template #default>
+        Feito por <UButton
+          icon="i-simple-icons-github"
+          color="neutral"
+          variant="ghost"
+          to="https://github.com/almino/unical"
+          target="_blank"
+          aria-label="GitHub"
+        /> Almino Melo
+      </template>
+    </UFooter>
   </UApp>
 </template>
